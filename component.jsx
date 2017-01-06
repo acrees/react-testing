05d681a57dfcfa44ba94cd6d99d6f1a495944f43
Alexander C Rees <me@acrees.co.uk>
2017-01-06 01:03:17 +0000

    adds full dom mounting and props and event support

--- component.jsx
+++ component.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default function Component() {
-  return (
-      <div>Test</div>
-  );
+function mapStateToProps(state) {
+  return {};
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick: () => dispatch({ type: 'on-click' })
+  };
+}
+
+class Component extends React.Component {
+  render() {
+    return (
+        <div>
+          <h1>Test</h1>
+          <button onClick={this.props.onClick}>Click me!</button>
+        </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Component);
--- component_test.jsx
+++ component_test.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Chai from 'chai';
 import Component from './component.jsx';
 
 Chai.should();
 
+class MockStore {
+  constructor() {
+    this.onClickActionWasDispatched = false;
+    this.dispatch = this._dispatch.bind(this);
+  }
+
+  subscribe() {}
+  getState() { return {}; }
+
+  _dispatch({type}) {
+    if (type === 'on-click') this.onClickActionWasDispatched = true;
+  }
+
+}
+
 describe('Component', () => {
   it('Component renders text', () => {
-    const sut = shallow(<Component />);
-    sut.find('div').text().should.equal('Test');
+    const mockStore = new MockStore();
+    const sut = mount(<Component store={mockStore} />);
+
+    sut.find('h1').text().should.equal('Test');
+    sut.find('button').text().should.equal('Click me!');
+  });
+  it('Dispatches an on-click action when clicked', () => {
+    const mockStore = new MockStore();
+    const sut = mount(<Component store={mockStore} />);
+
+    sut.find('button').simulate('click');
+
+    mockStore.onClickActionWasDispatched.should.be.true;
   });
 });
